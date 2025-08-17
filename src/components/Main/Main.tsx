95b3182d028b4f59e43acf71bdd4b86bd8600f10
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2025-08-17 08:49:37 +0300

    averaging find pitch

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,30 +10,13 @@ export type Props = {
 };
 type State = { pitches: number[] };
 
+const minClarity = 95;
+
 export function MusicSchool({ node, detector, rate }: Props) {
   const [state, dispatch] = useReducer(reducer, { pitches: [] });
   useEffect(() => {
-    async function findPitch(
-      node: AnalyserNode,
-      detector: Detector,
-      rate: number
-    ): Promise<number> {
-      let result = 0;
-      while (result == 0) {
-        result = await new Promise((resolve) =>
-          setTimeout(() => {
-            const input = new Float32Array(detector.inputLength);
-            node.getFloatTimeDomainData(input);
-            const [pitch, clarity] = detector.findPitch(input, rate);
-            console.log(`Pitch: ${pitch}, Clarity: ${clarity}`);
-            resolve(pitch);
-          }, 100)
-        );
-      }
-      return result;
-    }
     if (node && detector && rate) {
-      findPitch(node, detector, rate).then((pitch) => dispatch(pitch));
+      findPitch(node, detector, rate).then((pitch) => dispatch(Math.round(pitch)));
     }
   }, [node, detector, rate, state]);
   return (
@@ -53,4 +36,48 @@ function reducer(state: State, pitch: number): State {
   return {
     pitches: state.pitches.concat([pitch]),
   };
+}
+
+async function findPitch(node: AnalyserNode, detector: Detector, rate: number): Promise<number> {
+  const nextPitch = () => singlePitch(node, detector, rate);
+  let result: number | null = null;
+  while (result == null) {
+    await delay(50);
+    const pitch = nextPitch();
+    if (pitch == null) {
+      result = pitch;
+      continue;
+    }
+    result = await averagePitch(pitch, nextPitch);
+  }
+  return result;
+}
+
+async function averagePitch(
+  initialPitch: number,
+  nextPitch: () => number | null,
+): Promise<number> {
+  const pitches = [];
+  let latestPitch: number | null = initialPitch;
+  while (latestPitch != null) {
+    pitches.push(latestPitch);
+    await delay(50);
+    latestPitch = nextPitch();
+  }
+  return pitches.reduce((a, b) => a + b) / pitches.length;
+}
+
+function singlePitch(node: AnalyserNode, detector: Detector, rate: number): number | null {
+  const input = new Float32Array(detector.inputLength);
+  node.getFloatTimeDomainData(input);
+  const [pitch, clarity] = detector.findPitch(input, rate);
+  // console.log(`Pitch: ${pitch}, Clarity: ${clarity}`);
+  if (pitch == 0 || clarity * 100 <= minClarity) {
+    return null;
+  }
+  return pitch;
+}
+
+async function delay(millis: number) {
+  await new Promise<void>((resolve) => setTimeout(resolve, millis));
 }
