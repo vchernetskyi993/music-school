364487c16de65fb0ae91b3ac2608a82f27e6dd6e
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2025-08-30 16:42:24 +0300

    fix: tonejs - importing asynchronously

--- src/hooks/player.ts
+++ src/hooks/player.ts
@@ -1,18 +1,30 @@
-import { useMemo } from 'react';
-import * as Tone from 'tone';
+import { useState } from 'react';
 
 interface Player {
   playNote: (note: string) => Promise<void>;
 }
 
-const noteLength = '4n';
+const noteLength = '8n';
 
 export function usePlayer(): Player {
-  const audio = useMemo(() => new Tone.Synth().toDestination(), []);
+  const [cachedTone, setTone] = useState<any>();
+  const importTone = async () => {
+    const tone = await import('tone');
+    setTone(tone);
+    return tone;
+  };
+  const buildAudio = (tone: any) => {
+    const audio = new tone.Synth().toDestination();
+    setAudio(audio);
+    return audio;
+  };
+  const [cachedAudio, setAudio] = useState<any>();
   return {
-    playNote(note) {
+    async playNote(note) {
+      const tone = cachedTone || await importTone();
+      const audio = cachedAudio || buildAudio(tone);
       return new Promise((resolve) => {
-        Tone.start().then(() => {
+        tone.start().then(() => {
           audio.triggerAttackRelease(note, noteLength);
           audio.onsilence = () => resolve();
         });
