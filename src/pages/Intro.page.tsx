4d1e12c065a6e5bcda5732a110f581565f5ad8e7
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2026-05-26 14:07:38 +0300

    fix: study notes link on intro page (#13)

--- src/pages/Intro.page.tsx
+++ src/pages/Intro.page.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { generatePath, Link } from 'react-router-dom';
 import { List, ListItem, Stack, Text, Title } from '@mantine/core';
 import { pages } from '@/components/NavBar';
 
@@ -28,8 +28,11 @@ export function Intro() {
           instrument.
         </ListItem>
         <ListItem>
-          <Link to={pages.studyNotes.to}>{pages.studyNotes.label}</Link> - play a note displayed on
-          the screen. Useful to learn positions of single notes on the instrument.
+          <Link to={generatePath(pages.studyNotes.to, pages.studyNotes.params)}>
+            {pages.studyNotes.label}
+          </Link>{' '}
+          - play a note displayed on the screen. Useful to learn positions of single notes on the
+          instrument.
         </ListItem>
       </List>
       <Title order={2}>Study Modes</Title>
