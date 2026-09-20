f501b28aec5c5ae07b924e7c53444f73cfd9b15a
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2026-09-20 11:44:41 +0300

    build(deps): removed storybook (#31)

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,5 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   ...mantine,
-  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
-  {
-    files: ['**/*.story.tsx'],
-    rules: { 'no-console': 'off' },
-  }
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] },
 );
